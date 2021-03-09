c1029bb339607458f65db8602d4d9fa4dacdae9b
82109 <jwisgenius@naver.com>
2021-03-10 00:38:44 +0900

    lv3 solve

--- basic.py
+++ basic.py
@@ -91,7 +91,7 @@ def dijkstra(W):
                 v_near = i
 
         if v_near >= 0:
-            # 찾은 노드를 (현재위치, 현재위치에서 가까운 위치, 비용) 의 형태로 
+            # 찾은 노드를 (현재위치, 현재위치에서 가까운 위치, 비용) 의 형태로
             # 탐색배열에 넣어준다
             edge = (touch[v_near], v_near, W[touch[v_near]][v_near])
             F.append(edge)
@@ -129,7 +129,7 @@ def Nqueen():
             if is_possible(now_row, i, col):
                 # 만약, 이전에 작업한 것 중에서 대각선에 있거나 같은열에 있는게 없으면,
                 # 현재 진행 열에 행의 값을 넣어준다.
-                # 그리고 다음 열에서 같은 일을 진행해준다.
+                # 그리고 다음 열에서 같은 일을 진행 해준다.
                 col[now_row] = i
                 count += dfs(n, col, now_row + 1)
 
@@ -144,4 +144,170 @@ def Nqueen():
 
     return solution(4)
 
-print(Nqueen())
+# print(Nqueen())
+
+
+def sum_of_subsets_container():
+    w = [0, 3, 4, 5, 6]
+    subsets = []
+    W = 12
+
+    def sum_of_subsets(i, weights, total):
+        if is_promising(i, weights, total):
+            if weights == W:
+                print(subsets)
+            else:
+                subsets.append(w[i + 1])
+                sum_of_subsets(i + 1, weights + w[i + 1], total - w[i + 1])
+                subsets.pop()
+                sum_of_subsets(i + 1, weights, total - w[i + 1])
+
+    def is_promising(i, weight, total):
+        if (weight == W or weight + w[i + 1] <= W) and (weight + total >= W):
+            return True
+        return False
+
+    return sum_of_subsets(0, 0, sum(w))
+
+# (sum_of_subsets_container())
+
+
+def express_by_N_solution(N, number):
+    dp = [[0]]
+    dp += [[int(str(N) * i)] for i in range(1, 9)]
+    def recursive_func(i):
+        if i > 9 or i == 9 and number not in dp[8]:
+            return -1
+
+        if number in dp[i - 1]:
+            return i - 1
+
+        for k in range(1, i):
+            for left in dp[i - k]:
+                for right in dp[k]:
+                    if left > 0 and right > 0:
+                        divied = left // right > 0 and left // right or 0
+                        divied2 = right // left > 0 and right // left or 0
+                        minus = left - right > 0 and left - right or 0
+                        minus2 = right - left > 0 and right - left or 0
+                        plus = left + right > 0 and left + right or 0
+                        mul = left * right > 0 and left * right or 0
+                        dp[i] += list(x for x in set([divied, divied2, minus2, minus, plus, mul]) if x > 0)
+
+        return recursive_func(i + 1)
+
+    ans = recursive_func(0)
+
+    if ans <= 8:
+        return ans
+
+    return -1
+
+# print(express_by_N_solution(5, 31168))
+
+
+def int_triangle(triangle):
+    # dp = [[x for x in triangle[i]] for i in range(len(triangle))]
+
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] = triangle[i][j] + triangle[i - 1][j]
+            elif j == len(triangle[i]) - 1:
+                triangle[i][j] = triangle[i][j] + triangle[i - 1][j - 1]
+            else:
+                triangle[i][j] = triangle[i][j] + max(triangle[i - 1][j - 1], triangle[i - 1][j])
+
+
+    # def dfs(i, col):
+    #     if i == len(triangle):
+    #         return
+    #
+    #     dp[i][col] = max(dp[i][col], triangle[i][col] + dp[i - 1][col])
+    #     dfs(i + 1, col)
+    #
+    #     dp[i][col + 1] = max(dp[i][col + 1], triangle[i][col + 1] + dp[i - 1][col])
+    #     dfs(i + 1, col + 1)
+    #
+    # dfs(1, 0)
+    return max(triangle[-1])
+
+
+# print(int_triangle([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
+
+
+def go_to_school():
+    def solution(m, n, puddles):
+        dp = [[0 for _ in range(m)] for _ in range(n)]
+        dp[0][1] = 1
+        dp[1][0] = 1
+        puddles = [[x - 1, y - 1] for x, y in puddles]
+
+        for row in range(n):
+            for col in range(m):
+                if row > 0 or col > 0:
+                    if [col , row] not in puddles:
+                        if [col - 1, row] in puddles:
+                            dp[row][col] += dp[row - 1][col]
+                        elif [col, row - 1] in puddles:
+                            dp[row][col] += dp[row][col - 1]
+                        else:
+                            dp[row][col] += dp[row - 1][col] + dp[row][col - 1]
+        return dp[n -1][m - 1] % 1000000007
+
+    # print(solution(2, 2, []), 2)
+    # print(solution(3, 3, []), 6)
+    # print(solution(3, 3, [[2, 2]]), 2)
+    # print(solution(4, 3, [[1, 3], [3, 1]]), 7)
+    # print(solution(3, 3, [[1, 3]]), 5)
+    # print(solution(3, 3, [[1, 3], [3, 1]]), 4)
+    # print(solution(3, 3, [[1, 3], [3, 1], [2, 3]]), 2)
+    # print(solution(3, 3, [[1, 3], [3, 1], [2, 3], [2, 1]]), 1)
+    # print(solution(7, 4, [[2, 1], [2, 2], [2, 3], [4, 2], [4, 3], [4, 4], [6, 2], [6, 3]]), 0)
+    # print(solution(4, 4, [[3, 2], [2, 4]]), 7)
+    # print(solution(100, 100, []), 690285631)
+    # return solution(4, 3, [[2, 2]])
+#
+# print(go_to_school())
+
+
+def very_far_nodes():
+    def solution(n, edge):
+        import collections
+        queue = collections.deque({1})
+        visit = [False] * (n + 1)
+        visit[1] = True
+
+        graph = {}
+
+        for i in range(n):
+            graph[i + 1] = set()
+
+        for f, t in edge:
+            graph[f].add(t)
+            graph[t].add(f)
+
+        result = set()
+
+        while queue:
+            flag = False
+            for _ in range(len(queue)):
+                v = queue.popleft()
+                temp = [x for x in graph[v] if visit[x] == False]
+                queue.extend(temp)
+                for x in graph[v]:
+                    visit[x] = True
+
+                if len(temp) == 0:
+                    if not flag:
+                        result.add(v)
+                else:
+                    flag = True
+                    result = set()
+
+        return len(result)
+
+
+    return solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]])
+
+print(very_far_nodes())
